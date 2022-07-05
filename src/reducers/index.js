556f0404849da613999fb89129460dc0fd090df4
jsh031613 <63402731+bnbu@users.noreply.github.com>
2022-07-05 19:21:47 +0900

    add redux-persist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from "redux-persist";
+import storageSession from 'redux-persist/lib/storage/session';
 
 import user from './user'
+
+const persistConfig = {
+  key: "root",
+  // localStorage에 저장합니다.
+  storageSession,
+  // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
+  whitelist: ["user"]
+  // blacklist -> 그것만 제외합니다
+};
 
 const rootReducer = combineReducers({
   user
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
