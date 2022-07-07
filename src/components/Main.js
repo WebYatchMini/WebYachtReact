c0b3ddcbd7c4e5bcaa46e14417c8e2478e0c8fd0
jsh031613 <63402731+bnbu@users.noreply.github.com>
2022-07-07 14:42:47 +0900

    room refresh 기능 구현

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,13 +6,44 @@ import './Main.css'
 import * as userAction from "../actions/user"
 
 class Main extends Component {
+    state = {
+        roomArray: []
+    }
 
+    constructor(props) {
+        super(props);
+        this.handleRefresh();
+    }
+
+    handleRefresh = () => {
+        fetch('api/room/refresh')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                roomArray: data
+            })
+        })
+    }
+    handleCreate = () => {
+        
+    }
     handleLogout = () => {
         fetch('/api/logout')
     }
 
     render() {
         const { storeUid, storeNickname, storeWin, storeLose, storeLogin, resetStore } = this.props
+        let roomidx = 1
+        let roomList = Array.from(this.state.roomArray).map(room => (
+            <div className='room'>
+                <div className='password'>{room.locked ? <i className="bi bi-lock-fill"></i> : <i className="bi bi-unlock-fill"></i>}</div>
+                <div className='number'>{('000000' + roomidx.toString()).slice(-6)}</div>
+                <div className='title'>{room.title}</div>
+                <div className='people'>{room.currentPlayer} / 2</div>
+                <div className='state'>{room.started ? "게임중" : "대기중"}</div>
+            </div>
+        ));
         return(
             <div className='common'>
                 {(() => {
@@ -21,20 +52,21 @@ class Main extends Component {
                 <div className='container-fluid' id='container'>
                     <div className='test' id='lobby'>
                         <div className='roomIndex'>
-                            <div className='password'><i class="bi bi-lock-fill"></i> / <i class="bi bi-unlock-fill"></i></div>
+                            <div className='password'><i className="bi bi-lock-fill"></i> / <i className="bi bi-unlock-fill"></i></div>
                             <div className='number'>Room No.</div>
                             <div className='title'>Title</div>
                             <div className='people'>Player</div>
                             <div className='state'>State</div>
                         </div>
                         <div id='roomList'>
-                            <div className='room'>
-                                <div className='password'><i class="bi bi-lock-fill"></i></div>
+                            {roomList}
+                            {/* <div className='room'>
+                                <div className='password'><i className="bi bi-lock-fill"></i></div>
                                 <div className='number'>000001</div>
                                 <div className='title'>제목</div>
                                 <div className='people'>1 / 2</div>
                                 <div className='state'>대기중</div>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div className='test' id='myInfo'>
@@ -48,16 +80,16 @@ class Main extends Component {
                     <div className='test' id='etc'>
                         <div className='sideLogo'>SIDE MENU</div>
                         <ul>
-                            <li><button><i class="bi bi-door-open-fill"></i> ROOM JOIN</button></li>
-                            <li><button><i class="bi bi-arrow-clockwise"></i> LIST REFRESH</button></li>
-                            <li><button><i class="bi bi-file-plus-fill"></i> CREATE ROOM</button></li>
+                            <li><button><i className="bi bi-door-open-fill"></i> ROOM JOIN</button></li>
+                            <li><button><i className="bi bi-arrow-clockwise"></i> LIST REFRESH</button></li>
+                            <li><button><i className="bi bi-file-plus-fill"></i> CREATE ROOM</button></li>
                             <li><button onClick={() => {
-                                // this.handleLogout();
+                                this.handleLogout();
                                 resetStore();
                                 window.location.href = '/login';
                             }}
-                            ><i class="bi bi-box-arrow-right"></i> LOGOUT</button></li>
-                            <li><button><i class="bi bi-question-circle-fill"></i> HELP</button></li>
+                            ><i className="bi bi-box-arrow-right"></i> LOGOUT</button></li>
+                            <li><button><i className="bi bi-question-circle-fill"></i> HELP</button></li>
                         </ul>
                     </div>    
                 </div>
